69ad2dbb145c3c2702c7dcfe75fc0684da9fec64
Bastard <115501085+ankiwork@users.noreply.github.com>
2023-04-29 00:32:40 +0300

    Update Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -3,5 +3,10 @@ void Product::TabularOutput(Product* product, int i) {
 	std::cout << "=======================\n";
 	std::cout << "        Product        \n";
 	std::cout << "=======================\n";
+	std::cout << "Name:      |" << product[i].name;
+	std::cout << "Type:      |" << product[i].type << std::endl;
+	std::cout << "Quant:     |" << product[i].quant << std::endl;
+	std::cout << "Weight:    |" << product[i].weight << std::endl;
+	std::cout << "Cost:      |" << product[i].cost << std::endl;
 	std::cout << "=======================\n";
 }
